b315eb5f8b967e054939ba67f8d9e8954357b680
Dennis N <dupdennis@gmail.com>
2023-06-22 11:59:03 +0300

    Initial commit

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,11 @@
+import { Test } from '@nestjs/testing';
+import { AppModule } from 'src/app.module';
+
+describe('APP e2e', () => {
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+  });
+  it.todo('Test');
+});
